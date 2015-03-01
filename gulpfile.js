1b1c404082c42c8d4d194e0fee2c8da2aefc8162
ZombieAlex <zombiealex69@gmail.com>
2015-03-01 12:35:10 -0800

    Make compilation the gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee');
 
-gulp.task('logger-change', function(){
+gulp.task('default', function(){
     gulp.src('src/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
         .pipe(gulp.dest('lib'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.coffee', ['logger-change']);
+    gulp.watch('src/*.coffee', ['default']);
 });
